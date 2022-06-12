04307757e68d91ee11ba18ddf1aa22ebf290b986
Moin Khan <nurmoin05@gmail.com>
2022-06-12 11:26:54 +0600

    backend connected with database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,6 +9,26 @@ const app = express();
 //? middle were
 app.use(cors());
 app.use(express.json());
+
+//? Databse connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@nmkhan.b6ko8eg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+//? Server stablish
+const server = async () => {
+    try {
+        client.connect();
+        const database = client.db('nmkhan');
+        const projectCollection = database.collection('projects')
+        console.log("database connected")
+    }
+
+    finally {
+        //// client.close();
+    }
+}
+
+server().catch(console.dir)
 
 
 //? listening to port
